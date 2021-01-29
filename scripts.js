e9a8dd29debda889d1021e6dc798c2f825ea8431
Luiz Felipe <luiz.silva16@estudante.ufla.br>
2021-01-29 00:06:31 -0300

    data-table inteligente

---
+++ scripts.js
@@ -0,0 +1,86 @@
+const Modal = {
+  open() {
+    document
+      .querySelector(".modal-overlay")
+      .classList
+      .add("active");
+  },
+  close() {
+    document
+      .querySelector(".modal-overlay")
+      .classList
+      .remove("active");
+  },
+}
+
+const transactions = [
+  {
+  id: 1,
+  description: "Luz",
+  amount: -50000,
+  date: '23/01/2021'
+  },
+  {
+  id: 2,
+  description: "Website",
+  amount: 500000,
+  date: '23/01/2021'
+  },
+  {
+  id: 2,
+  description: "Internet",
+  amount: -20000,
+  date: '23/01/2021'
+  }
+]
+
+const Transaction = {
+  incomes() {
+    // sum the incomes
+  },
+  expenses() {
+    // sum the expenses
+  },
+  total() {
+    // incomes - expenses
+  }
+}
+
+// Eu preciso substituir os dados do HTML com
+// os dados do JS
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+    
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class="${CSSclass}">${transaction.amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td>
+      <img src="./assets/minus.svg" alt="Remover Transação" />
+    </td>
+    `
+
+    return html
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+  }
+}
+
+transactions.forEach(function(transaction){
+  DOM.addTransaction(transaction)
+})
