897e71aeb6294fee80628520c6e09c0d4ea99cfd
Luiz Felipe <luiz.silva16@estudante.ufla.br>
2021-02-15 20:11:10 -0300

    :tada: logo animation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+const tagSVG = document.querySelector('svg');
+
+
+const pathDoSVG = document.querySelector('svg path');
+const tamanhoTotalDoPath = pathDoSVG.getTotalLength();
+tagSVG.style.setProperty('--tamanhoTotalDoPath', tamanhoTotalDoPath);
+
 const Modal = {
   open() {
     document
